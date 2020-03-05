80b3585c12d6f9039853b2e5e07e31e8ad72a0f7
Arthur <kom9toshka@gmail.com>
2020-03-05 14:17:05 +0300

    add: count-cats-decided

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,14 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix = []) {
+  
+  let count = [];
+  matrix.reduce(
+    (acc, curr) => (acc.concat([...curr])).map((i)=>{
+    return (i == "^^")
+      ? count.push(i)
+      : 0
+  }), [])
+    
+  return (count.length != 0 )
+    ? count.length
+    : 0
 };
