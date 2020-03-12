cdedc66dc2bf47ec940e8ac9c640fed757988996
Arthur <kom9toshka@gmail.com>
2020-03-12 13:29:13 +0300

    add: solution-carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,24 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string') {
+    return false
+  }
+
+  if (sampleActivity.split('.').length > 2) {
+    const arr = sampleActivity.split('.')
+    sampleActivity = `${arr[0]}.${arr[1]}`
+  }
+  
+  const number = Number(sampleActivity)
+
+  if (Number.isNaN(number) || number <= 0 || number > 15) {
+    return false
+  }
+
+  const k = Math.log(2).toFixed(3) / HALF_LIFE_PERIOD
+  const t = Math.log(MODERN_ACTIVITY / sampleActivity) / k
+
+  return Math.ceil(t)
 };
--- src/count-cats.js
+++ src/count-cats.js
@@ -1,14 +1,24 @@
 module.exports = function countCats(matrix = []) {
-  
-  let count = [];
-  matrix.reduce(
-    (acc, curr) => (acc.concat([...curr])).map((i)=>{
-    return (i == "^^")
-      ? count.push(i)
-      : 0
-  }), [])
-    
-  return (count.length != 0 )
-    ? count.length
-    : 0
+  const result = matrix.reduce((acc, subArray) => {
+    const c = subArray.filter(i => i == '^^').length;
+    return acc + c;
+  }, 0);
+
+  return result;
 };
+
+
+// module.exports = function countCats(matrix = []) {
+  
+//   let count = [];
+//   matrix.reduce(
+//     (acc, curr) => (acc.concat([...curr])).map((i)=>{
+//     return (i == "^^")
+//       ? count.push(i)
+//       : 0
+//   }), [])
+    
+//   return (count.length != 0 )
+//     ? count.length
+//     : 0
+// };
